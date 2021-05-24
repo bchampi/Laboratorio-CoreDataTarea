45e23ce1b0808ba8a170d653ec7c3b52237a488c
Mac 17 <brayan.champi@tecsup.edu.pe>
2021-05-23 22:53:21 -0500

    Eliminación de comentarios innecesarios

--- CoreDataTarea/TableViewController.swift
+++ CoreDataTarea/TableViewController.swift
@@ -40,51 +40,16 @@ class TableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         let course = courses[indexPath.row]
-        let result = (course.examAverage * 0.30) + (course.labAverage * 0.70)
+        let result: Double = (course.examAverage * 0.30) + (course.labAverage * 0.70)
         if result < 13.0 {
             cell.backgroundColor = UIColor.red
             cell.textLabel?.text = "\(course.name!) - \(result)"
         } else {
             cell.backgroundColor = UIColor.green
-            cell.textLabel?.text = "\(course.name!) - \(result)"
+            cell.textLabel?.text = "\(course.name!) - \(round(result * 100) / 100.0)"
         }
         return cell
     }
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let course = courses[indexPath.row]
